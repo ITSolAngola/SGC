421a4a83063c6bf9e1cff2c9736a6e51e05b77af
unknown <testeaprendendo2018@gmail.com>
2018-11-14 11:32:00 +0100

    novo

---
+++ src/main/java/pro/it/gestao_clinica/GestaoClinicaApplication.java
@@ -0,0 +1,12 @@
+package pro.it.gestao_clinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoClinicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoClinicaApplication.class, args);
+    }
+}
